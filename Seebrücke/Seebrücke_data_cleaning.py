ea07b50b20bc5078f3153d7cbe080ee86d14190b
Olfairn <flo.marandet@gmail.com>
2021-07-21 15:35:40 +0200

    add last transfert files and more companies

--- Seebrücke/Seebrücke_data_cleaning.py
+++ Seebrücke/Seebrücke_data_cleaning.py
@@ -1,9 +1,15 @@
 
 #%%
+from numpy import NaN
 import pandas as pd
 from pandas.core.algorithms import value_counts
 from pandas import to_numeric, to_datetime, get_dummies
 import random
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import axes, axis, figure, title
+import pylab as pl
+import re
+plt.rcParams["figure.figsize"] = (22,14)
 
 #%%
 twingle1 = pd.read_csv('data/01_a_twingle_Seebrücke_2018-2019.csv',sep=';')
@@ -12,12 +18,12 @@ twingle3 = pd.read_csv('data/01_c_twingle_Seebücke_2020_2.csv',sep=';')
 twingle_dauer = pd.read_csv('data/02_twingle_Dauerspende.csv',sep=';')
 twingle_Winter = pd.read_csv('data/03_twingle_Winterkampagne.csv',sep=';')
 twingle_united = pd.read_csv('data/04_twingle_UnitedWeShare.csv',sep=';')
-#TODO Add older Fundbox to data
+#TODO Check if Fundbox data contains everythin
 Fundbox_2020 = pd.read_csv('data/FundraisingBox_Donations_2020_non_anonymized.csv',sep=';',encoding='latin1')
 
-#TODO Sort donation from grants (in bank transfert)
 Transfert_1 = pd.read_excel('data/Kontoauszüge Seebrücke 2020.xlsx') #Everything is incoming
 Transfert_2 = pd.read_excel('data/Kontoauszüge 2018-2019.xlsx',header=1) #INcoming => +
+Transfert_3 = pd.read_excel('data/new_konto_2020.xlsx',parse_dates=True) #INcoming => +
 
 #%%
 # Select usefull twingle data
@@ -53,6 +59,7 @@ Transfert_1['Kontoinhaber*in'].value_counts()
 
 #Get rid of outgoing transfert  
 Transfert_2 = Transfert_2.loc[Transfert_2[' + '] > 0].copy()
+Transfert_3 = Transfert_3.loc[Transfert_3['Soll/Haben'] == 'H'].copy()
 
 #Clean the amount col 
 Transfert_1['Betrag'] = Transfert_1['Betrag'].str.replace('Euro','')
@@ -60,20 +67,32 @@ Transfert_1['Betrag'] = Transfert_1['Betrag'].str.replace(' ','')
 Transfert_1['Betrag'] = Transfert_1['Betrag'].str.replace(',','')
 Transfert_1['Betrag'] = to_numeric(Transfert_1['Betrag'])
 
-Transfert_2 = Transfert_2[['Unnamed: 0',' + ','Stadt']]
-Transfert_2.columns = ['donated_at','amount','city']
+Transfert_2 = Transfert_2[['Unnamed: 0',' + ','Stadt','Unnamed: 1']]
+Transfert_2.columns = ['donated_at','amount','city','company_name']
 
-Transfert_1 = Transfert_1[['Datum','Betrag']]
-Transfert_1.columns = ['donated_at','amount']
+Transfert_1 = Transfert_1[['Datum','Betrag','Kontoinhaber*in']]
+Transfert_1.columns = ['donated_at','amount','company_name']
+
+Transfert_3.columns = ['donated_at','address','company_name','currency','amount','in/out']
+Transfert_3 = Transfert_3[['donated_at','amount','company_name']]
+
 
 #%%
-#There are nulls in the date col ... 
-Transfert_df = pd.concat([Transfert_1,Transfert_2],ignore_index=True)
+Transfert_df = pd.concat([Transfert_1,Transfert_2,Transfert_3],ignore_index=True)
 Transfert_df['donated_at'] = to_datetime(Transfert_df['donated_at'],errors='coerce')
 Transfert_df = Transfert_df.dropna(subset=['donated_at'])
 
 Transfert_df['by_recurring'] = 0
-Transfert_df['email_address'] = random.sample(range(1,10000),1021)
+Transfert_df['email_address'] = random.sample(range(1,10000),len(Transfert_df))
+
+#%%
+#* Drop verein donations
+verein_index = Transfert_df[Transfert_df['company_name'].str.contains('e\. V',flags=re.IGNORECASE)==True].index
+Transfert_df.drop(verein_index,inplace=True)
+
+#%%
+#Keep just companies in the company col
+Transfert_df['company_name'] = Transfert_df.company_name[Transfert_df['company_name'].str.contains('gmbh',flags=re.IGNORECASE)==True]
 
 #%%
 #Merge Twingle and Fundbox data
@@ -92,21 +111,43 @@ df['person_id'] = codes
 df.drop('email_address',axis=1,inplace=True)
 
 #%%
+df['company_name'] = df['company_name'].notnull().astype('int')
+df.rename(columns={'company_name':'company'})
+#%%
 df.to_csv('data_seebrücke_anno.csv')
 
 
 
 #%%
---------
 #! Fundbox gives person_id
-#Fundbox1 = pd.read_csv('data/05_FundraisingBox_Seebruecke_Spende.csv',sep=';')
-#Fundbox2 = pd.read_csv('data/06_FundraisingBox_Seebuecke_donations.csv',sep=';')
-#Fundbox3 = pd.read_csv('data/07_FundraisingBox_Seebruecke_foerdern_Newsletter.csv',sep=';')
-#Fundbox4 = pd.read_csv('data/08_FundraisingBox_Seebruecke_Winterkampagne.csv',sep=';')
-#Fundbox5 = pd.read_csv('data/09_FundraisingBox_Seebruecke_winter_donation_campaign.csv',sep=';')
+Fundbox1 = pd.read_csv('data/05_FundraisingBox_Seebruecke_Spende.csv',sep=';')
+Fundbox2 = pd.read_csv('data/06_FundraisingBox_Seebuecke_donations.csv',sep=';')
+Fundbox3 = pd.read_csv('data/07_FundraisingBox_Seebruecke_foerdern_Newsletter.csv',sep=';')
+Fundbox4 = pd.read_csv('data/08_FundraisingBox_Seebruecke_Winterkampagne.csv',sep=';')
+Fundbox5 = pd.read_csv('data/09_FundraisingBox_Seebruecke_winter_donation_campaign.csv',sep=';')
 #! Strange one
 #Fundbox6 = pd.read_csv('data/10_FundraisingBox_Seebruecke_foerdern_Social_Media.csv',sep=';',encoding='cp1252')
-#Fundbox7 = pd.read_csv('data/11_FundraisingBox_Seebruecke_foerdern_der_freitag.csv',sep=';')
+Fundbox7 = pd.read_csv('data/11_FundraisingBox_Seebruecke_foerdern_der_freitag.csv',sep=';')
 # %%
 #Transfert_df_5000 = Transfert_df.loc[Transfert_df['amount'] >= 5000]
 #Transfert_df_5000
+
+fund_df = pd.concat([Fundbox1,Fundbox2,Fundbox3,Fundbox4,Fundbox5,Fundbox7])
+
+#%%
+fund_df.amount.sum()
+#%%
+Fundbox_df.amount.sum()
+#%%
+
+small_fund = fund_df[['donated_at','amount']]
+
+#%%
+small_fund.donated_at = to_datetime(small_fund.donated_at)
+#%%
+small_fund.donated_at.hist()
+pl.suptitle("Old Fundbox")
+#%%
+Fundbox_df.donated_at.hist()
+pl.suptitle("New Fundbox Data")
+
